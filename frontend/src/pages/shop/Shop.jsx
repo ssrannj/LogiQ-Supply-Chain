4b7855d332495611923776d16805077fec82051b
ssrannj <jeyanthansaran@gmail.com>
2026-03-18 23:41:38 +0530

    chore: initialize catalog page structure

---
+++ frontend/src/pages/shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div className="shop-container">
+      <h1>Shop Catalog</h1>
+      <p>Welcome to the LogiQ Supply Chain Catalog. Please select a product.</p>
+    </div>
+  );
+};
+
+export default Shop;
